79c3a82623da3f5af3c632550d46659606a8923f
mataktelis11 <mataktelis01@gmail.com>
2022-04-10 17:33:49 +0300

    Finished readme for randgraph

--- randgraph.py
+++ randgraph.py
@@ -1,28 +1,22 @@
-import random
-import time
+from random import uniform
+from time import perf_counter
 
 
 def createRandomGraph(n, k):
 
-	E = [0]*k # make E global for more effeciency
+	E = [0]*k # make E global for more efficiency
 
-	def calculate(n, k):
-		
-		N = (n - 1)*n / 2
-		t = 0
-		for i in range(1,n+1):
-			for j in range(i+1,n+1):
-				U = random.uniform(0, 1)
-				if (N * U < k):
-					E[t]=(i,j)
-					k = k - 1
-					if(k==0):return E
-					t = t + 1
-				N = N - 1
-
-	return(calculate(n,k))
-
-
+	N = (n - 1)*n / 2
+	t = 0
+	for i in range(1,n+1):
+		for j in range(i+1,n+1):
+			U = uniform(0, 1)
+			if (N * U < k):
+				E[t]=(i,j)
+				k = k - 1
+				if(k==0):return E
+				t = t + 1
+			N = N - 1
 
 
 
@@ -33,10 +27,9 @@ def createRandomGraph(n, k):
 n = 5
 k = 4
 #####################
-t0= time.time()
+t0= perf_counter()
 E = createRandomGraph(n, k)
-t1 = time.time() - t0
+t1 = perf_counter() - t0
 print("Time elapsed",t1)
 print(E)
 '''
-
